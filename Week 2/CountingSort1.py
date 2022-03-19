890a5f627e1cba97d27dee1abdd430760ff0934b
Bhomik Kinger <69043188+bhomik33@users.noreply.github.com>
2022-03-19 21:54:26 +1100

    Create CountingSort1.py

---
+++ Week 2/CountingSort1.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # initialize a frequencyArray with the initial value of zero of the elements
+    freqArrray = [0] * 100
+    for i in range(0,len(arr)):
+        freqArrray[arr[i]] += 1
+    return freqArrray
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
